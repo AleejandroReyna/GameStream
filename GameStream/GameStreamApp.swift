b01f74f7a7cc98a93992e187d7874ec16f2c14b0
Alejandro Reyna <aleejandroreyna@gmail.com>
2023-03-19 23:57:41 -0600

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Alejandro Reyna on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
