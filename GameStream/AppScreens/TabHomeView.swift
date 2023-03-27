52c1d5d05bc6417f8f8f2977ac31bc277c5b8c8b
Alejandro Reyna <aleejandroreyna@gmail.com>
2023-03-26 23:39:48 -0600

    home-view: Added popular sub module

--- GameStream/AppScreens/TabHomeView.swift
+++ GameStream/AppScreens/TabHomeView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct TabHomeView: View {
     @State var searchText = ""
@@ -41,6 +42,10 @@ struct TabHomeView: View {
                 .padding([.top, .leading, .bottom], 11)
                 .background(Color("Light-Blue"))
                 .clipShape(Capsule())
+                
+                ScrollView {
+                    PopularSubModule()
+                }
             }
             .padding(.horizontal, 10)
             
@@ -49,6 +54,57 @@ struct TabHomeView: View {
     
     func search() -> Void {
         print("User is looking for \(searchText)")
+    }
+}
+
+struct PopularSubModule:View {
+    @State var url = "https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4"
+    @State var isPlayerActive = false
+    let urlVideos:[String] = ["https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256671638/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256720061/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256814567/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256705156/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256801252/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256757119/movie480.mp4"]
+    
+    var body : some View {
+        VStack {
+            Text("MOST POPULAR")
+                .font(.title3)
+                .foregroundColor(.white)
+                .bold()
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                .padding(.top)
+            
+            Button {
+                print(url)
+                isPlayerActive = true
+            } label: {
+                ZStack {
+
+                    VStack(spacing: 0) {
+                        Image("Default-Featured-Image").resizable().scaledToFill()
+                        Text("The Witcher")
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding()
+                            .bold()
+                            .font(.title3)
+                            .background(Color("Light-Blue"))
+                    }
+                    
+                    Image(systemName: "play.circle.fill")
+                        .resizable()
+                        .foregroundColor(.white)
+                        .frame(width: 42, height: 42)
+                    
+                }.frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                 .padding(.vertical)
+            }
+        }
+        
+        NavigationLink(value: "showVideo") {
+            EmptyView()
+        }
+        .navigationDestination(isPresented: $isPlayerActive) {
+            VideoPlayer(player: AVPlayer(url: URL(string: url)!))
+                .frame(width: 400, height: 300)
+        }
+
     }
 }
 
