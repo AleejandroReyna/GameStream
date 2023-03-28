1811e3b0a4e0257da9fc5def0961ef94c8062989
Alejandro Reyna <aleejandroreyna@gmail.com>
2023-03-28 00:20:09 -0600

    games-view: Added detail gallery

--- GameStream/AppScreens/GameView.swift
+++ GameStream/AppScreens/GameView.swift
@@ -22,6 +22,8 @@ struct GameView: View {
                     ScrollView {
                         DetailInfo(game: detailGame)
                             .padding(.bottom)
+                        
+                        DetailGallery(game: detailGame)
                     }
                     .frame(maxWidth: .infinity)
                 }
@@ -90,7 +92,44 @@ struct DetailInfo : View {
     }
 }
 
-let testGame : GameParser = GameParser(title: "Sonic", studio: "Sega", contentRaiting: "E+", publicationYear: "1991", description: "Juego de Sega Genesis publicado en 1991 con más de 40 millones de copias vendidas actualmente", platforms: ["PC"], tags: ["plataformas","mascota"], videosUrls: VideosUrlsParser(mobile: "https://dl.dropboxusercontent.com/s/k6g0zwmsxt9qery/TheWitcher480.mp4", tablet: "https://dl.dropboxusercontent.com/s/9faic5dtebavp1o/TheWitcherMax.mp4"), galleryImages: [ "https://cdn.cloudflare.steamstatic.com/steam/apps/268910/ss_615455299355eaf552c638c7ea5b24a8b46e02dd.600x338.jpg","https://cdn.cloudflare.steamstatic.com/steam/apps/268910/ss_615455299355eaf552c638c7ea5b24a8b46e02dd.600x338.jpg","https://cdn.cloudflare.steamstatic.com/steam/apps/268910/ss_615455299355eaf552c638c7ea5b24a8b46e02dd.600x338.jpg"])
+struct DetailGallery : View {
+    var game : GameParser
+    
+    var body : some View {
+        VStack(alignment: .leading) {
+            Text("Gallery")
+                .padding(.horizontal, 10)
+                .foregroundColor(.white)
+                .font(.title)
+                .bold()
+            
+            ScrollView(.horizontal) {
+                LazyHGrid(rows: gridShape, spacing: 8) {
+                    ForEach(game.galleryImages!, id: \.self) { url in
+                        AsyncImage(url: URL(string: url)) { Image in
+                            Image
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                        } placeholder: {
+                            Text("loading...")
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+            }.frame(height: 180)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+let gridShape = [
+    GridItem(.flexible())
+]
+
+let testGame : GameParser = GameParser(title: "Sonic", studio: "Sega", contentRaiting: "E+", publicationYear: "1991", description: "Juego de Sega Genesis publicado en 1991 con más de 40 millones de copias vendidas actualmente", platforms: ["PC"], tags: ["plataformas","mascota"], videosUrls: VideosUrlsParser(mobile: "https://dl.dropboxusercontent.com/s/k6g0zwmsxt9qery/TheWitcher480.mp4", tablet: "https://dl.dropboxusercontent.com/s/9faic5dtebavp1o/TheWitcherMax.mp4"),
+   galleryImages: [ "https://cdn.cloudflare.steamstatic.com/steam/apps/292030/ss_107600c1337accc09104f7a8aa7f275f23cad096.600x338.jpg",
+       "https://cdn.cloudflare.steamstatic.com/steam/apps/292030/ss_ed23139c916fdb9f6dd23b2a6a01d0fbd2dd1a4f.600x338.jpg",
+       "https://cdn.cloudflare.steamstatic.com/steam/apps/292030/ss_908485cbb1401b1ebf42e3d21a860ddc53517b08.600x338.jpg"])
 
 struct GameView_Previews: PreviewProvider {
     
