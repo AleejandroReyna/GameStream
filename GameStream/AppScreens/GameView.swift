d92e3baf5ff3cb8457b7ea550659afdc9a16d19b
Alejandro Reyna <aleejandroreyna@gmail.com>
2023-03-28 13:00:23 -0600

    games-view: Added related games section

--- GameStream/AppScreens/GameView.swift
+++ GameStream/AppScreens/GameView.swift
@@ -8,6 +8,18 @@
 import SwiftUI
 import AVKit
 
+
+let testGame : GameParser = GameParser(title: "Sonic", studio: "Sega", contentRaiting: "E+", publicationYear: "1991", description: "Juego de Sega Genesis publicado en 1991 con más de 40 millones de copias vendidas actualmente", platforms: ["PC"], tags: ["plataformas","mascota"], videosUrls: VideosUrlsParser(mobile: "https://dl.dropboxusercontent.com/s/k6g0zwmsxt9qery/TheWitcher480.mp4", tablet: "https://dl.dropboxusercontent.com/s/9faic5dtebavp1o/TheWitcherMax.mp4"),
+   galleryImages: [ "https://cdn.cloudflare.steamstatic.com/steam/apps/292030/ss_107600c1337accc09104f7a8aa7f275f23cad096.600x338.jpg",
+       "https://cdn.cloudflare.steamstatic.com/steam/apps/292030/ss_ed23139c916fdb9f6dd23b2a6a01d0fbd2dd1a4f.600x338.jpg",
+       "https://cdn.cloudflare.steamstatic.com/steam/apps/292030/ss_908485cbb1401b1ebf42e3d21a860ddc53517b08.600x338.jpg"])
+
+let demoImages = [
+    "https://cdn.cloudflare.steamstatic.com/steam/apps/292030/ss_107600c1337accc09104f7a8aa7f275f23cad096.600x338.jpg",
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/292030/ss_ed23139c916fdb9f6dd23b2a6a01d0fbd2dd1a4f.600x338.jpg",
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/292030/ss_908485cbb1401b1ebf42e3d21a860ddc53517b08.600x338.jpg"
+]
+
 struct GameView: View {
     var game : GameParser?
     
@@ -27,6 +39,9 @@ struct GameView: View {
                             .padding(.bottom, 30)
                         
                         DetailComments()
+                            .padding(.bottom, 30)
+                        
+                        DetailRelated()
                     }
                     .frame(maxWidth: .infinity)
                 }
@@ -139,6 +154,37 @@ struct DetailComments : View {
     }
 }
 
+struct DetailRelated : View {
+    var body : some View {
+        VStack(alignment: .leading) {
+            Text("Related Games")
+                .padding(.horizontal, 10)
+                .foregroundColor(.white)
+                .font(.title2)
+                .bold()
+            
+            ScrollView(.horizontal) {
+                LazyHGrid(rows: gridShape, spacing: 8) {
+                    ForEach(demoImages, id: \.self) { url in
+                        Button {
+                            print(url)
+                        } label: {
+                            AsyncImage(url: URL(string: url)) { Image in
+                                Image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            } placeholder: {
+                                EmptyView()
+                            }
+                        }
+                    }
+                }
+            }.frame(height: 180)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
 let gridShape = [
     GridItem(.flexible())
 ]
@@ -177,11 +223,6 @@ struct Comment : View {
         }.frame(maxWidth: .infinity)
     }
 }
-
-let testGame : GameParser = GameParser(title: "Sonic", studio: "Sega", contentRaiting: "E+", publicationYear: "1991", description: "Juego de Sega Genesis publicado en 1991 con más de 40 millones de copias vendidas actualmente", platforms: ["PC"], tags: ["plataformas","mascota"], videosUrls: VideosUrlsParser(mobile: "https://dl.dropboxusercontent.com/s/k6g0zwmsxt9qery/TheWitcher480.mp4", tablet: "https://dl.dropboxusercontent.com/s/9faic5dtebavp1o/TheWitcherMax.mp4"),
-   galleryImages: [ "https://cdn.cloudflare.steamstatic.com/steam/apps/292030/ss_107600c1337accc09104f7a8aa7f275f23cad096.600x338.jpg",
-       "https://cdn.cloudflare.steamstatic.com/steam/apps/292030/ss_ed23139c916fdb9f6dd23b2a6a01d0fbd2dd1a4f.600x338.jpg",
-       "https://cdn.cloudflare.steamstatic.com/steam/apps/292030/ss_908485cbb1401b1ebf42e3d21a860ddc53517b08.600x338.jpg"])
 
 struct GameView_Previews: PreviewProvider {
     
