f73f9ac7222139733df215076f730a683fdc80ab
Alejandro Reyna <aleejandroreyna@gmail.com>
2023-03-28 22:02:32 -0600

    favorites-view: Added favorites view
    With default swift ui view

---
+++ GameStream/AppScreens/TabFavoritesView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesView.swift
+//  GameStream
+//
+//  Created by Alejandro Reyna on 28/03/23.
+//
+
+import SwiftUI
+
+struct TabFavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabFavoritesView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabFavoritesView()
+    }
+}
