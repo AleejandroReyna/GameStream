1d8eacff4606593f43226ca738ef809b3e9aa09e
Alejandro Reyna <aleejandroreyna@gmail.com>
2023-03-29 01:19:45 -0600

    profile-view: Added module form

--- GameStream/AppScreens/EditProfileView.swift
+++ GameStream/AppScreens/EditProfileView.swift
@@ -28,17 +28,91 @@ struct EditProfileView: View {
                             
                         }
                     }
+                    .padding(.bottom, 30)
+                    
+                    EditProfileForm()
                 }
-                
-                EditProfileForm()
             }
         }
     }
 }
 
 struct EditProfileForm : View {
+    @State var email : String = ""
+    @State var password : String = ""
+    @State var name : String = ""
+    
     var body : some View {
-        Text("Edit")
+        VStack(alignment: .leading) {
+            
+            Text("Email")
+                .foregroundColor(Color("Dark-Cyan"))
+                .bold()
+                
+            TextField(
+                text: $email,
+                label: {
+                    Text("text@example.com").foregroundColor(.gray)
+                })
+                .foregroundColor(.white)
+            
+            Divider()
+                .frame(height: 1)
+                .background(Color("Dark-Cyan"))
+                .padding(.bottom)
+            
+            Text("Password")
+            .foregroundColor(Color("Dark-Cyan"))
+            .bold()
+            
+            SecureField(
+                text: $password,
+                label: {
+                    Text("New Password").foregroundColor(.gray)
+                })
+                .foregroundColor(.white)
+        
+            Divider()
+                .frame(height: 1)
+                .background(Color("Dark-Cyan"))
+                .padding(.bottom)
+            
+            Text("Name")
+                .foregroundColor(Color("Dark-Cyan"))
+                .bold()
+                
+            TextField(
+                text: $name,
+                label: {
+                    Text("Your name here...").foregroundColor(.gray)
+                })
+                .foregroundColor(.white)
+            
+            Divider()
+                .frame(height: 1)
+                .background(Color("Dark-Cyan"))
+                .padding(.bottom, 60)
+            
+            Button {
+                updateData()
+            } label: {
+                Text("UPDATE")
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding(EdgeInsets(top: 11, leading: 10, bottom: 11, trailing: 10))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 6)
+                            .stroke(Color("Dark-Cyan"), lineWidth: 1)
+                            .shadow(color: .white, radius: 6)
+                    )
+            }
+            
+        }.padding(.horizontal, 30)
+    }
+    
+    func updateData() -> Void {
+        print("updating")
     }
 }
 
